d744e198388da21312127fd566ee65759ab86ee1
Aleksandr Birkov <birkov.alexandr@gmail.com>
2022-08-11 15:07:28 +0400

    add TypegooseModule

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,14 +1,46 @@
-export class ProductModel {
+import { Prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+class ProductCharacteristic {
+  name: string;
+  value: string;
+}
+
+export interface ProductModel extends Base {}
+export class ProductModel extends TimeStamps {
+  @Prop()
   image: string;
+
+  @Prop()
   title: string;
+
+  @Prop()
   price: number;
+
+  @Prop()
   oldPrice: number;
+
+  @Prop()
   credit: number;
+
+  @Prop()
   calculatedRating: number;
+
+  @Prop()
   decsription: string;
+
+  @Prop()
   advantages: string;
+
+  @Prop()
   disadvantages: string;
+
+  @Prop({ type: () => [String] })
   categories: string[];
-  tags: string;
-  characteristics: Record<string, string>;
+
+  @Prop({ type: () => [String] })
+  tags: string[];
+
+  @Prop({ type: () => [ProductCharacteristic], _id: false })
+  characteristics: ProductCharacteristic[];
 }
--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,20 @@
-export class ReviewModel {
-  _id: string;
+import { Prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+export interface ReviewModel extends Base {}
+export class ReviewModel extends TimeStamps {
+  @Prop()
   name: string;
+
+  @Prop()
   title: string;
+
+  @Prop()
   description: string;
+
+  @Prop()
   rating: number;
+
+  @Prop()
   createdAt: Date;
 }
