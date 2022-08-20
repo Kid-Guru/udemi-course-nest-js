d2b2981686a8a8b83d13fc244ed039804640c383
Aleksandr Birkov <birkov.alexandr@gmail.com>
2022-08-20 12:01:37 +0400

    add IdValidationPipe

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -11,6 +11,7 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { IdValidationPipe } from 'src/pipes/id-validateion.pipe';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { UserEmail } from '../decorators/user-email.decorator';
 import { REVIEW_NOT_FOUND } from './constants';
@@ -29,7 +30,7 @@ export class ReviewController {
 
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  async delete(@Param('id') id: string) {
+  async delete(@Param('id', IdValidationPipe) id: string) {
     const deletedDoc = await this.reviewService.delete(id);
 
     if (!deletedDoc) {
@@ -40,7 +41,7 @@ export class ReviewController {
   @UseGuards(JwtAuthGuard)
   @Get('byProduct/:productId')
   async getByProduct(
-    @Param('productId') productId: string,
+    @Param('productId', IdValidationPipe) productId: string,
     @UserEmail() email: string,
   ) {
     console.log(email);
